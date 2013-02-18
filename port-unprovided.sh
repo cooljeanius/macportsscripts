9022f2a163f7138b4b467ea9c41962bb5d62b9aa
System Administrator <root@gl00b05046.cats.gwu.edu>
2013-02-18 14:22:09 -0500

    changes to port-unprovided.sh
    - add "--help" flag that describes usage and several flags that I
    intend to add
    - add "-r" flag that recurses deep into your MacPorts prefix

--- port-unprovided.sh
+++ port-unprovided.sh
@@ -7,15 +7,40 @@ if [ -z "`which port`" ]; then
     exit 0
 fi
 
+if [ "$1" == "--help" ]; then
+	echo "Usage:"
+	echo ""
+	echo "--help					use this flag to display this help."
+	echo "-v					use this flag to be verbose (doesn't actually work yet)."
+	echo "-r					use this flag to recurse deep into your prefix."
+	echo "--prefix=/placeholder/path/to/prefix	use this flag to select which prefix to use (defaults to /opt/local) (not actually implemented yet)."
+	echo ""
+	echo "Warning: only the first flag specified is actually recognized so far"
+	echo ""
+	exit 0
+fi
+
 if [ -d /opt/local ]; then
-	for directory in /opt/local/*
-	do
-		if [ -z "`port provides ${directory}/* | grep "is not provided by a MacPorts port."`" ]; then
-			echo "${directory}: no unprovided files found here"
-		else
-			port provides ${directory}/* | grep "is not provided by a MacPorts port."
-		fi
-	done
+	if [ "$1" == "-r" ]; then
+		echo "Generating list files in prefix, this might take a while..."
+		for directory in `find /opt/local/*`; do
+			if [ -d ${directory} ]; then
+				if [ -z "`port provides ${directory}/* | grep "is not provided by a MacPorts port."`" ]; then
+					echo "${directory}: no unprovided files found here"
+				else
+					port provides ${directory}/* | grep "is not provided by a MacPorts port."
+				fi
+			fi
+		done
+	else
+		for directory in /opt/local/*; do
+			if [ -z "`port provides ${directory}/* | grep "is not provided by a MacPorts port."`" ]; then
+				echo "${directory}: no unprovided files found here"
+			else
+				port provides ${directory}/* | grep "is not provided by a MacPorts port."
+			fi
+		done
+	fi
 else
 	echo "This script does not support alternate prefixes yet, sorry."
 fi
